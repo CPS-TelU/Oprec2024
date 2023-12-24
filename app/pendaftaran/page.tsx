4bcd6e58d339a144783a98325a1a9d56a2d577d6
Hanntuu <raihanrrdarmawan@gmail.com>
2023-12-24 15:09:11 +0700

    update regis

--- app/pendaftaran/page.tsx
+++ app/pendaftaran/page.tsx
@@ -1,19 +1,21 @@
-// "use client";
-// import React from "react";
-// import Form from "@/components/formulir";
-// import NavForm from "@/components/navbarform";
+"use client";
+import React from "react";
+import Form from "@/components/formulir";
+import NavForm from "@/components/navbarform";
+import Fullform from "@/components/fullform";
 
-// const page = () => {
-//   return (
-//     <>
-//       <div className="sticky top-0 z-50">
-//         <NavForm />
-//       </div>
-//       <div className="-mt-[30px] md:-mt-[20px] lg:mt-[30px]">
-//         <Form />
-//       </div>
-//     </>
-//   );
-// };
+const page = () => {
+  return (
+    <>
+      {/* <div className="sticky top-0 z-50">
+        <NavForm />
+      </div>
+      <div className="-mt-[30px] md:-mt-[20px] lg:mt-[30px]">
+        <Form />
+      </div> */}
+      <Fullform/>
+    </>
+  );
+};
 
-// export default page;
+export default page;
