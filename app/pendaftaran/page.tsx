36354d998aeb9ed56da9411ea90fabe45bd3e62e
Hanntuu <raihanrrdarmawan@gmail.com>
2023-12-24 15:04:59 +0700

    close regis

--- app/pendaftaran/page.tsx
+++ app/pendaftaran/page.tsx
@@ -1,19 +1,19 @@
-"use client";
-import React from "react";
-import Form from "@/components/formulir";
-import NavForm from "@/components/navbarform";
+// "use client";
+// import React from "react";
+// import Form from "@/components/formulir";
+// import NavForm from "@/components/navbarform";
 
-const page = () => {
-  return (
-    <>
-      <div className="sticky top-0 z-50">
-        <NavForm />
-      </div>
-      <div className="-mt-[30px] md:-mt-[20px] lg:mt-[30px]">
-        <Form />
-      </div>
-    </>
-  );
-};
+// const page = () => {
+//   return (
+//     <>
+//       <div className="sticky top-0 z-50">
+//         <NavForm />
+//       </div>
+//       <div className="-mt-[30px] md:-mt-[20px] lg:mt-[30px]">
+//         <Form />
+//       </div>
+//     </>
+//   );
+// };
 
-export default page;
+// export default page;
